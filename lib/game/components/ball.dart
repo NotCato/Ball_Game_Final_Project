f1a8a0e0d6c2340b23d14baa5b32e75c2e6c69d1
Cristiano Monteiro <cristiano.monteiro.2025091@my.istec.pt>
2026-07-22 22:19:25 +0100

    Criação de um spawnpoint

--- lib/game/components/ball.dart
+++ lib/game/components/ball.dart
@@ -3,20 +3,21 @@ import 'package:flame_forge2d/flame_forge2d.dart';
 import 'spike.dart';
 
 class Ball extends BodyComponent with CollisionCallbacks{
-  Ball() {
+  
+  final Vector2 spawnPoint;
+  
+  Ball(this.spawnPoint) {
     debugMode = true;
     priority = 10; // Garante que a bola é desenhada por cima do mapa
   }
 
-  double posx;
-  double posy;
 
   @override
   Body createBody() {
     // 1. Define as propriedades físicas do corpo.
     final bodyDef = BodyDef(
       type: BodyType.dynamic, // Dynamic = afetado por forças e gravidade.
-      position: Vector2(5.0, 50.0), // Posição inicial no canto inferior esquerdo.
+      position: spawnPoint, // Posição inicial no canto inferior esquerdo.
       linearDamping: 0.5,  // Adiciona "atrito" com o ar/mesa para parar a bola.
       angularDamping: 0.5, // Adiciona atrito na rotação.
       allowSleep: false,   // Mantém a bola acordada para responder à gravidade.
@@ -39,7 +40,7 @@ class Ball extends BodyComponent with CollisionCallbacks{
   }
 
   void die(){
-    body.setTransform(Vector2(posx, posy), 0.0);
+    body.setTransform(spawnPoint, 0.0);
     body.linearVelocity = Vector2.zero();
     body.angularVelocity = 0;
   }
