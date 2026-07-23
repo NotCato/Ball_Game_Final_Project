b8b562ee9383e7e4a266863b8d3d658f8a51f198
Cristiano Monteiro <cristiano.monteiro.2025091@my.istec.pt>
2026-07-23 19:02:36 +0100

    nivel 2 do tiled, adição dos visuais dos espinhos

---
+++ assets/images/espinho.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="espinho" tilewidth="56" tileheight="56" tilecount="1" columns="1">
+ <image source="../../../../Sprite-0001.png" width="56" height="56"/>
+</tileset>
