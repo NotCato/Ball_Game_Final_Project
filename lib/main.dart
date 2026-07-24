d98002fa0677e6215ed2507e3c8e313ac2bbd4f7
Cristiano Monteiro <cristiano.monteiro.2025091@my.istec.pt>
2026-07-24 19:36:42 +0100

    Implementação do Firebase Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'presentation/screens/menu.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeLeft,
